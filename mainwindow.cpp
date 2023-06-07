7d9ddaf02e84be85be608210d0b4d6b381dd52cc
XuanInsr <2087868649@qq.com>
2023-06-08 02:49:51 +0800

    完成了部分按钮和快捷键

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,22 +1,93 @@
 #include "mainwindow.h"
+#include <QPdfWriter>
+#include <QTextDocumentWriter>
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QClipboard>
 
 void MainWindow::fileNew() {
+    qDebug() << "新建文件";
+    if (!checkFileSave())
+        return;
 
+    textEdit->clear();
+    filePath.reset();
+    setWindowTitle("未命名");
+    qDebug() << "新建文件完成";
+//    document->addResource(QTextDocument::ImageResource, QUrl(":/icons/file-new.png"),
+//                          QImage(":/icons/file-new.png"));
+//    QTextCursor cursor(document);
+//    cursor.movePosition(QTextCursor::End);
+//    QTextImageFormat imageFormat;
+//    imageFormat.setName(":/icons/file-new.png");
+//    cursor.insertImage(imageFormat);
 }
 
 void MainWindow::fileOpen() {
+    qDebug() << "打开文件";
 
+    if (!checkFileSave())
+        return;
+
+    QString path = QFileDialog::getOpenFileName(this, "打开文件", QString(), "HTML Files (*.html)");
+
+    if (!path.isEmpty()) {
+        QFile file(path);
+
+        if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+            QTextStream in(&file);
+            QString content = in.readAll();
+
+            document->setHtml(content);
+            filePath = path;
+
+            qDebug() << QString{"文件 %1 打开成功"}.arg(path);
+        } else {
+            qWarning() << QString{"文件 %1 打开失败"}.arg(path);
+        }
+    } else {
+        qWarning() << "打开 - 用户没有选择路径";
+    }
 }
 
 void MainWindow::fileSave() {
+    qDebug() << "保存文件";
 
+    if (!filePath)
+        return fileSaveAs();
+
+    QTextDocumentWriter writer(filePath.value());
+    writer.setFormat("HTML");
+
+    bool success = writer.write(document);
+    if (success)
+        document->setModified(false);
+
+    qDebug() << QString{"保存文件 %1"}.arg(success ? "成功" : "失败");
 }
 
 void MainWindow::fileSaveAs() {
+    qDebug() << "另存为";
+    QString path = QFileDialog::getSaveFileName(this, "存储为", QString(), "HTML Files (*.html)");
 
+    if (!path.isEmpty()) {
+        QTextDocumentWriter writer(path);
+        writer.setFormat("HTML");  // 设置保存格式为 HTML
+
+        bool success = writer.write(document);  // 将当前文档保存为 HTML
+        if (success) {
+            document->setModified(false);
+            updateFilePath(path);
+        }
+
+        qDebug() << QString{"另存为 %1 %2"}.arg(path, success ? "成功" : "失败");
+    } else {
+        qWarning() << "另存为 - 用户没有选择路径";
+    }
 }
 
 void MainWindow::fileExport() {
+    qDebug() << "导出为 PDF";
 //    QPrinter printer(QPrinter::PrinterResolution);
 //    printer.setOutputFormat(QPrinter::PdfFormat);
 //    printer.setOutputFileName("output.pdf");  // 设置输出文件名
@@ -25,18 +96,50 @@ void MainWindow::fileExport() {
 //    painter.begin(&printer);
 //    document->drawContents(&painter);
 //    painter.end();
+
+    QPdfWriter writer("out.pdf");
+    writer.setPageSize(QPageSize(QPageSize::A4));
+    writer.setResolution(150);
+
+    QPainter painter(&writer);
+    document->drawContents(&painter);
+    painter.end();
+    qDebug() << "导出完成";
 }
 
 void MainWindow::styleBold() {
+    QTextCursor cursor = textEdit->textCursor(); // 获取文本光标
+    QTextCharFormat charFormat = cursor.charFormat(); // 获取当前字符格式
 
+    // 检查当前是否已应用加粗样式
+    if (charFormat.fontWeight() == QFont::Bold) {
+        charFormat.setFontWeight(QFont::Normal); // 改为不加粗
+    } else {
+        charFormat.setFontWeight(QFont::Bold); // 改为加粗
+    }
+
+    cursor.mergeCharFormat(charFormat); // 合并字符格式
+    textEdit->setTextCursor(cursor); // 更新文本光标
 }
 
 void MainWindow::styleItalic() {
+    QTextCursor cursor = textEdit->textCursor(); // 获取文本光标
+    QTextCharFormat charFormat = cursor.charFormat(); // 获取当前字符格式
 
+    charFormat.setFontItalic(!charFormat.fontItalic());
+
+    cursor.mergeCharFormat(charFormat); // 合并字符格式
+    textEdit->setTextCursor(cursor); // 更新文本光标
 }
 
 void MainWindow::styleUnderline() {
+    QTextCursor cursor = textEdit->textCursor(); // 获取文本光标
+    QTextCharFormat charFormat = cursor.charFormat(); // 获取当前字符格式
 
+    charFormat.setFontUnderline(!charFormat.fontUnderline());
+
+    cursor.mergeCharFormat(charFormat); // 合并字符格式
+    textEdit->setTextCursor(cursor); // 更新文本光标
 }
 
 void MainWindow::styleSize() {
@@ -51,28 +154,19 @@ void MainWindow::styleFont() {
 
 }
 
-void MainWindow::alignLeft() {
-
-}
-
-void MainWindow::alignCenter() {
-
-}
-
-void MainWindow::alignRight() {
-
-}
-
 void MainWindow::editPaste() {
-
+    textEdit->paste();
+    qDebug() << "粘贴: " << QGuiApplication::clipboard()->text();
 }
 
 void MainWindow::editCopy() {
-
+    textEdit->copy();
+    qDebug() << "拷贝: " << QGuiApplication::clipboard()->text();
 }
 
 void MainWindow::editCut() {
-
+    textEdit->cut();
+    qDebug() << "剪切: " << QGuiApplication::clipboard()->text();
 }
 
 void MainWindow::editImage() {
@@ -95,10 +189,38 @@ void MainWindow::utilReplace() {
 
 }
 
-void MainWindow::utilZoomIn() {
+bool MainWindow::checkFileSave() {
+    qDebug() << "检查是否更改：" << (document->isModified() ? "是" : "否");
+    if (document->isModified()) {
+        auto answer = QMessageBox::question(this, "是否保存当前更改？",
+                                            "是否保存当前更改？", QMessageBox::Yes | QMessageBox::No | QMessageBox::Cancel);
 
+        if (answer == QMessageBox::Yes) {
+            qDebug() << "用户选择保存";
+            fileSave();
+            return true;
+        } else if (answer == QMessageBox::No) {
+            qDebug() << "用户选择不保存";
+            return true;
+        } else {  // answer == QMessageBox::Cancel
+            qDebug() << "用户选择取消";
+            return false;
+        }
+    }
+    return true;
 }
 
-void MainWindow::utilZoomOut() {
+void MainWindow::updateFilePath(const QString &path) {
+    filePath = path;
+    setWindowTitle(path);
+}
 
+void MainWindow::setAlignment(Qt::AlignmentFlag alignment) {
+    QTextCursor cursor = textEdit->textCursor(); // 获取文本编辑框的光标
+    QTextBlockFormat blockFormat = cursor.blockFormat(); // 获取当前段落的格式
+
+    blockFormat.setAlignment(alignment); // 设置对齐方式
+
+    cursor.setBlockFormat(blockFormat); // 应用段落格式
+    textEdit->setTextCursor(cursor); // 更新文本编辑框的光标
 }
--- mainwindow.h
+++ mainwindow.h
@@ -5,31 +5,48 @@
 #include <QTextEdit>
 #include <QPrinter>
 #include <QPainter>
+#include <QCloseEvent>
+#include <QShortcut>
 
 class MainWindow : public QMainWindow {
 public:
     explicit MainWindow(QWidget *parent = nullptr) : QMainWindow(parent) {
+        setWindowTitle("未命名");
+
         // 创建文本编辑框
-        textEdit = new QTextEdit(this);
+        textEdit = new MyTextEdit(this);
         setCentralWidget(textEdit);
 
         // 将 textEdit 和 document 关联；此后对一方的更改会在另一方同步
         textEdit->setDocument(document);
+        textEdit->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Expanding);
 
         // 初始化工具栏
         initToolbars();
     }
 
+    void closeEvent(QCloseEvent* event) override {
+        if (checkFileSave()) {
+            event->accept();  // 允许关闭窗口
+        } else {
+            event->ignore();  // 忽略关闭事件
+        }
+    }
+
 private:
     // new 一个 QAction 并设置其 Icon, Tip 和 Slot
     template<typename Func>
-    QAction * initQAction(const QString &filename, const QString &tip, Func slot) {
+    QAction * initQAction(const QString &filename, const QString &tip, Func slot, std::optional<QKeySequence> shortcut) {
         auto * btn = new QAction(this);
 
         btn->setIcon(QIcon{filename});
 //        qDebug() << QString("已设置按钮 [%1] 的图标，图标 [%2] 空").arg(tip, (btn->icon().isNull() ? "为" : "不为"));
 
         btn->setToolTip(tip);
+
+        if (shortcut)
+            btn->setShortcut(shortcut.value());
+
         connect(btn, &QAction::triggered, this, slot);
         return btn;
     }
@@ -37,42 +54,42 @@ private:
     void initToolbars() {
         qDebug() << "初始化工具栏：initToolbars";
         auto * filesTb = new QToolBar(this);
-        filesTb->addAction(initQAction(":/icons/file-new.png", "新建文件 (Ctrl+N)", &MainWindow::fileNew));
-        filesTb->addAction(initQAction(":/icons/file-open.png", "打开文件 (Ctrl+O)", &MainWindow::fileOpen));
-        filesTb->addAction(initQAction(":/icons/file-save.png", "保存文件 (Ctrl+S)", &MainWindow::fileSave));
-        filesTb->addAction(initQAction(":/icons/file-saveas.png", "另存为", &MainWindow::fileSaveAs));
-        filesTb->addAction(initQAction(":/icons/file-export.png", "导出为 PDF", &MainWindow::fileExport));
+        filesTb->addAction(initQAction(":/icons/file-new.png", "新建文件 (Ctrl+N)", &MainWindow::fileNew, QKeySequence(Qt::CTRL | Qt::Key_N)));
+        filesTb->addAction(initQAction(":/icons/file-open.png", "打开文件 (Ctrl+O)", &MainWindow::fileOpen, QKeySequence(Qt::CTRL | Qt::Key_O)));
+        filesTb->addAction(initQAction(":/icons/file-save.png", "保存文件 (Ctrl+S)", &MainWindow::fileSave, QKeySequence(Qt::CTRL | Qt::Key_S)));
+        filesTb->addAction(initQAction(":/icons/file-saveas.png", "另存为", &MainWindow::fileSaveAs, {}));
+        filesTb->addAction(initQAction(":/icons/file-export.png", "导出为 PDF", &MainWindow::fileExport, {}));
         addToolBar(filesTb);
 
         auto * stylesTb = new QToolBar(this);
-        stylesTb->addAction(initQAction(":/icons/style-bold.png", "加粗 (Ctrl+B)", &MainWindow::styleBold));
-        stylesTb->addAction(initQAction(":/icons/style-italic.png", "斜体 (Ctrl+I)", &MainWindow::styleItalic));
-        stylesTb->addAction(initQAction(":/icons/style-underline.png", "下划线 (Ctrl+U)", &MainWindow::styleUnderline));
-        stylesTb->addAction(initQAction(":/icons/style-size.png", "字号", &MainWindow::styleSize));
-        stylesTb->addAction(initQAction(":/icons/style-color.png", "颜色", &MainWindow::styleColor));
-        stylesTb->addAction(initQAction(":/icons/style-font.png", "字体", &MainWindow::styleFont));
+        stylesTb->addAction(initQAction(":/icons/style-bold.png", "加粗 (Ctrl+B)", &MainWindow::styleBold, QKeySequence(Qt::CTRL | Qt::Key_B)));
+        stylesTb->addAction(initQAction(":/icons/style-italic.png", "斜体 (Ctrl+I)", &MainWindow::styleItalic, QKeySequence(Qt::CTRL | Qt::Key_I)));
+        stylesTb->addAction(initQAction(":/icons/style-underline.png", "下划线 (Ctrl+U)", &MainWindow::styleUnderline, QKeySequence(Qt::CTRL | Qt::Key_U)));
+        stylesTb->addAction(initQAction(":/icons/style-size.png", "字号", &MainWindow::styleSize, {}));
+        stylesTb->addAction(initQAction(":/icons/style-color.png", "颜色", &MainWindow::styleColor, {}));
+        stylesTb->addAction(initQAction(":/icons/style-font.png", "字体", &MainWindow::styleFont, {}));
         addToolBar(stylesTb);
 
         auto * alignsTb = new QToolBar(this);
-        alignsTb->addAction(initQAction(":/icons/align-left.png", "左对齐 (Ctrl+L)", &MainWindow::alignLeft));
-        alignsTb->addAction(initQAction(":/icons/align-center.png", "居中 (Ctrl+E)", &MainWindow::alignCenter));
-        alignsTb->addAction(initQAction(":/icons/align-right.png", "右对齐 (Ctrl+R)", &MainWindow::alignRight));
+        alignsTb->addAction(initQAction(":/icons/align-left.png", "左对齐 (Ctrl+L)", &MainWindow::alignLeft, QKeySequence(Qt::CTRL | Qt::Key_L)));
+        alignsTb->addAction(initQAction(":/icons/align-center.png", "居中 (Ctrl+E)", &MainWindow::alignCenter, QKeySequence(Qt::CTRL | Qt::Key_E)));
+        alignsTb->addAction(initQAction(":/icons/align-right.png", "右对齐 (Ctrl+R)", &MainWindow::alignRight, QKeySequence(Qt::CTRL | Qt::Key_R)));
         addToolBar(alignsTb);
 
         auto * editTb = new QToolBar(this);
-        editTb->addAction(initQAction(":/icons/edit-paste.png", "粘贴 (Ctrl+V)", &MainWindow::editPaste));
-        editTb->addAction(initQAction(":/icons/edit-copy.png", "拷贝 (Ctrl+C)", &MainWindow::editCopy));
-        editTb->addAction(initQAction(":/icons/edit-cut.png", "剪切 (Ctrl+X)", &MainWindow::editCut));
-        editTb->addAction(initQAction(":/icons/edit-image.png", "插入图片", &MainWindow::editImage));
-        editTb->addAction(initQAction(":/icons/edit-undo.png", "撤销 (Ctrl+Z)", &MainWindow::editUndo));
-        editTb->addAction(initQAction(":/icons/edit-redo.png", "重做 (Ctrl+Y)", &MainWindow::editRedo));
+        editTb->addAction(initQAction(":/icons/edit-paste.png", "粘贴 (Ctrl+V)", &MainWindow::editPaste, QKeySequence(Qt::CTRL | Qt::Key_V)));
+        editTb->addAction(initQAction(":/icons/edit-copy.png", "拷贝 (Ctrl+C)", &MainWindow::editCopy, QKeySequence(Qt::CTRL | Qt::Key_C)));
+        editTb->addAction(initQAction(":/icons/edit-cut.png", "剪切 (Ctrl+X)", &MainWindow::editCut, QKeySequence(Qt::CTRL | Qt::Key_X)));
+        editTb->addAction(initQAction(":/icons/edit-image.png", "插入图片", &MainWindow::editImage, {}));
+        editTb->addAction(initQAction(":/icons/edit-undo.png", "撤销 (Ctrl+Z)", &MainWindow::editUndo, QKeySequence(Qt::CTRL | Qt::Key_Z)));
+        editTb->addAction(initQAction(":/icons/edit-redo.png", "重做 (Ctrl+Y)", &MainWindow::editRedo, QKeySequence(Qt::CTRL | Qt::Key_Y)));
         addToolBar(editTb);
 
         auto * utilsTb = new QToolBar(this);
-        utilsTb->addAction(initQAction(":/icons/util-find.png", "查找 (Ctrl+F)", &MainWindow::utilFind));
-        utilsTb->addAction(initQAction(":/icons/util-replace.png", "替换 (Ctrl+R)", &MainWindow::utilReplace));
-        utilsTb->addAction(initQAction(":/icons/util-zoomin.png", "放大", &MainWindow::utilZoomIn));
-        utilsTb->addAction(initQAction(":/icons/util-zoomout.png", "缩小", &MainWindow::utilZoomOut));
+        utilsTb->addAction(initQAction(":/icons/util-find.png", "查找 (Ctrl+F)", &MainWindow::utilFind, QKeySequence(Qt::CTRL | Qt::Key_F)));
+        utilsTb->addAction(initQAction(":/icons/util-replace.png", "替换 (Ctrl+R)", &MainWindow::utilReplace, QKeySequence(Qt::CTRL | Qt::Key_R)));
+        utilsTb->addAction(initQAction(":/icons/util-zoomin.png", "放大", &MainWindow::utilZoomIn, QKeySequence(Qt::CTRL | Qt::Key_Plus)));
+        utilsTb->addAction(initQAction(":/icons/util-zoomout.png", "缩小", &MainWindow::utilZoomOut, QKeySequence(Qt::CTRL | Qt::Key_Minus)));
         addToolBar(utilsTb);
 
         qDebug() << "初始化工具栏：initToolbars 完成";
@@ -92,9 +109,9 @@ private slots:
     void styleColor();
     void styleFont();
 
-    void alignLeft();
-    void alignCenter();
-    void alignRight();
+    void alignLeft() { setAlignment(Qt::AlignLeft); }
+    void alignCenter() { setAlignment(Qt::AlignCenter); }
+    void alignRight() { setAlignment(Qt::AlignRight); }
 
     void editPaste();
     void editCopy();
@@ -105,8 +122,8 @@ private slots:
 
     void utilFind();
     void utilReplace();
-    void utilZoomIn();
-    void utilZoomOut();
+    void utilZoomIn() { textEdit->zoomIn(); }
+    void utilZoomOut() { textEdit->zoomOut(); }
 
     void insertText1() {
         QTextCursor cursor(document);
@@ -121,19 +138,35 @@ private slots:
 
     void insertText2() {
 //        auto *textEdit = qobject_cast<QTextEdit*>(centralWidget());
-        document->addResource(QTextDocument::ImageResource, QUrl(":/icons/file-new.png"),
-                              QImage(":/icons/file-new.png"));
-        QTextCursor cursor(document);
-        cursor.movePosition(QTextCursor::End);
-        QTextImageFormat imageFormat;
-        imageFormat.setName(":/icons/file-new.png");
-        cursor.insertImage(imageFormat);
+
 
         if (textEdit)
             textEdit->insertPlainText("文本2 ");
     }
 
 private:
+    class MyTextEdit : public QTextEdit {
+    public:
+        explicit MyTextEdit(QWidget *parent = nullptr) : QTextEdit(parent) {}
+        void customPaste() {}
+    protected:
+        void keyPressEvent(QKeyEvent *event) override {
+            if (event->modifiers() == Qt::ControlModifier && event->key() == Qt::Key_V) {
+                // 处理 Ctrl + V 快捷键
+                customPaste();
+                return;
+            }
+
+            // 其他情况下，继续处理默认的键盘事件
+            QTextEdit::keyPressEvent(event);
+        }
+    };
+
     QTextDocument *document = new QTextDocument();
-    QTextEdit *textEdit;
+    MyTextEdit *textEdit;
+    std::optional<QString> filePath{};
+
+    bool checkFileSave();   // 询问是否保存当前文件，如果为是或否，返回 true。返回 false 表示取消。
+    void updateFilePath(const QString &path);
+    void setAlignment(Qt::AlignmentFlag alignment);
 };
