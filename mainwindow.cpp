fe5e1096fe6ac9e18cc1de029deb62c92ec6dd0a
XuanInsr <2087868649@qq.com>
2023-06-07 22:59:20 +0800

    图标显示完成

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,22 @@
 #include "mainwindow.h"
 
-//void MainWindow::fileExport() {
+void MainWindow::fileNew() {
+
+}
+
+void MainWindow::fileOpen() {
+
+}
+
+void MainWindow::fileSave() {
+
+}
+
+void MainWindow::fileSaveAs() {
+
+}
+
+void MainWindow::fileExport() {
 //    QPrinter printer(QPrinter::PrinterResolution);
 //    printer.setOutputFormat(QPrinter::PdfFormat);
 //    printer.setOutputFileName("output.pdf");  // 设置输出文件名
@@ -9,4 +25,80 @@
 //    painter.begin(&printer);
 //    document->drawContents(&painter);
 //    painter.end();
-//}
+}
+
+void MainWindow::styleBold() {
+
+}
+
+void MainWindow::styleItalic() {
+
+}
+
+void MainWindow::styleUnderline() {
+
+}
+
+void MainWindow::styleSize() {
+
+}
+
+void MainWindow::styleColor() {
+
+}
+
+void MainWindow::styleFont() {
+
+}
+
+void MainWindow::alignLeft() {
+
+}
+
+void MainWindow::alignCenter() {
+
+}
+
+void MainWindow::alignRight() {
+
+}
+
+void MainWindow::editPaste() {
+
+}
+
+void MainWindow::editCopy() {
+
+}
+
+void MainWindow::editCut() {
+
+}
+
+void MainWindow::editImage() {
+
+}
+
+void MainWindow::editUndo() {
+
+}
+
+void MainWindow::editRedo() {
+
+}
+
+void MainWindow::utilFind() {
+
+}
+
+void MainWindow::utilReplace() {
+
+}
+
+void MainWindow::utilZoomIn() {
+
+}
+
+void MainWindow::utilZoomOut() {
+
+}
--- mainwindow.h
+++ mainwindow.h
@@ -9,16 +9,6 @@
 class MainWindow : public QMainWindow {
 public:
     explicit MainWindow(QWidget *parent = nullptr) : QMainWindow(parent) {
-        // 创建工具栏
-        auto *toolbar = new QToolBar(this);
-        addToolBar(toolbar);
-
-        // 创建工具按钮
-        auto *toolButton1 = new QAction("按钮1", this);
-        auto *toolButton2 = new QAction("按钮2", this);
-        toolbar->addAction(toolButton1);
-        toolbar->addAction(toolButton2);
-
         // 创建文本编辑框
         textEdit = new QTextEdit(this);
         setCentralWidget(textEdit);
@@ -26,10 +16,7 @@ public:
         // 将 textEdit 和 document 关联；此后对一方的更改会在另一方同步
         textEdit->setDocument(document);
 
-        // 连接工具按钮的信号与槽函数
-//        connect(toolButton1, &QAction::triggered, this, &MainWindow::insertText1);
-//        connect(toolButton2, &QAction::triggered, this, &MainWindow::insertText2);
-
+        // 初始化工具栏
         initToolbars();
     }
 
@@ -40,7 +27,7 @@ private:
         auto * btn = new QAction(this);
 
         btn->setIcon(QIcon{filename});
-        qDebug() << QString("已设置按钮 [%1] 的图标，图标 [%2] 空").arg(tip, (btn->icon().isNull() ? "为" : "不为"));
+//        qDebug() << QString("已设置按钮 [%1] 的图标，图标 [%2] 空").arg(tip, (btn->icon().isNull() ? "为" : "不为"));
 
         btn->setToolTip(tip);
         connect(btn, &QAction::triggered, this, slot);
@@ -56,15 +43,47 @@ private:
         filesTb->addAction(initQAction(":/icons/file-saveas.png", "另存为", &MainWindow::fileSaveAs));
         filesTb->addAction(initQAction(":/icons/file-export.png", "导出为 PDF", &MainWindow::fileExport));
         addToolBar(filesTb);
+
+        auto * stylesTb = new QToolBar(this);
+        stylesTb->addAction(initQAction(":/icons/style-bold.png", "加粗 (Ctrl+B)", &MainWindow::styleBold));
+        stylesTb->addAction(initQAction(":/icons/style-italic.png", "斜体 (Ctrl+I)", &MainWindow::styleItalic));
+        stylesTb->addAction(initQAction(":/icons/style-underline.png", "下划线 (Ctrl+U)", &MainWindow::styleUnderline));
+        stylesTb->addAction(initQAction(":/icons/style-size.png", "字号", &MainWindow::styleSize));
+        stylesTb->addAction(initQAction(":/icons/style-color.png", "颜色", &MainWindow::styleColor));
+        stylesTb->addAction(initQAction(":/icons/style-font.png", "字体", &MainWindow::styleFont));
+        addToolBar(stylesTb);
+
+        auto * alignsTb = new QToolBar(this);
+        alignsTb->addAction(initQAction(":/icons/align-left.png", "左对齐 (Ctrl+L)", &MainWindow::alignLeft));
+        alignsTb->addAction(initQAction(":/icons/align-center.png", "居中 (Ctrl+E)", &MainWindow::alignCenter));
+        alignsTb->addAction(initQAction(":/icons/align-right.png", "右对齐 (Ctrl+R)", &MainWindow::alignRight));
+        addToolBar(alignsTb);
+
+        auto * editTb = new QToolBar(this);
+        editTb->addAction(initQAction(":/icons/edit-paste.png", "粘贴 (Ctrl+V)", &MainWindow::editPaste));
+        editTb->addAction(initQAction(":/icons/edit-copy.png", "拷贝 (Ctrl+C)", &MainWindow::editCopy));
+        editTb->addAction(initQAction(":/icons/edit-cut.png", "剪切 (Ctrl+X)", &MainWindow::editCut));
+        editTb->addAction(initQAction(":/icons/edit-image.png", "插入图片", &MainWindow::editImage));
+        editTb->addAction(initQAction(":/icons/edit-undo.png", "撤销 (Ctrl+Z)", &MainWindow::editUndo));
+        editTb->addAction(initQAction(":/icons/edit-redo.png", "重做 (Ctrl+Y)", &MainWindow::editRedo));
+        addToolBar(editTb);
+
+        auto * utilsTb = new QToolBar(this);
+        utilsTb->addAction(initQAction(":/icons/util-find.png", "查找 (Ctrl+F)", &MainWindow::utilFind));
+        utilsTb->addAction(initQAction(":/icons/util-replace.png", "替换 (Ctrl+R)", &MainWindow::utilReplace));
+        utilsTb->addAction(initQAction(":/icons/util-zoomin.png", "放大", &MainWindow::utilZoomIn));
+        utilsTb->addAction(initQAction(":/icons/util-zoomout.png", "缩小", &MainWindow::utilZoomOut));
+        addToolBar(utilsTb);
+
         qDebug() << "初始化工具栏：initToolbars 完成";
     }
 
 private slots:
-    void fileNew() { qDebug() << "fileNew"; }
-    void fileOpen() { qDebug() << "fileNew"; }
-    void fileSave() { qDebug() << "fileNew"; }
-    void fileSaveAs() { qDebug() << "fileNew"; }
-    void fileExport() { qDebug() << "fileNew"; }
+    void fileNew();
+    void fileOpen();
+    void fileSave();
+    void fileSaveAs();
+    void fileExport();
 
     void styleBold();
     void styleItalic();
